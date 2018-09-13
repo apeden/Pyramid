13d3c49b92d61dd1528b7a114ed555478bc4cc00
apeden <apeden23@gmail.com>
2018-09-13 01:15:07 +0100

    me uploading pyramid, attempt at first assignment learning Java

---
+++ src/pyramid.java
@@ -0,0 +1,19 @@
+/* 
+ * File: pyramid.java
+ * ------------------------
+ * This is my attempt at assignment 2, question 1 (Stanford CSS 2007)
+ * The goal is to draw a pyramid of bricks
+ * 
+ *
+ * 
+ */
+
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class pyramid extends GraphicsProgram {
+	public void run() {
+		add(new GLabel("hello world", 100, 75));
+	}
+}
