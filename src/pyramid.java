7b0ce813ffb31475821f7fbb84055e2ca74d7e9e
apeden <apeden23@gmail.com>
2018-09-15 00:53:22 +0100

    making some progress with getting bricks onto the canvas

--- src/pyramid.java
+++ src/pyramid.java
@@ -4,16 +4,19 @@
  * This is my attempt at assignment 2, question 1 (Stanford CSS 2007)
  * The goal is to draw a pyramid of bricks
  * 
- *
- * 
  */
-
+import java.awt.Color;
 
 import acm.graphics.*;
 import acm.program.*;
 
 public class pyramid extends GraphicsProgram {
+/* I must be able to change the following values*/
 	public void run() {
-		add(new GLabel("hello world", 100, 75));
+		int BRICK_WIDTH = 30;
+		int BRICK_HEIGHT = 12;
+		int BRICKS_IN_BASE = 14;
+		GRect brick = new GRect(BRICK_WIDTH,BRICK_HEIGHT);
+		add(brick);
 	}
 }
